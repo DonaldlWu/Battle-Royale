439e0022a71e41e846a1f34fbdf24e7d13fcdf07
CoalaCola <vince9458@yahoo.com.tw>
2017-09-08 11:09:59 +0800

    random coordinate and mapView topAnchor

---
+++ MapViewController.swift
@@ -0,0 +1,145 @@
+//
+//  ViewController.swift
+//  Battle Royale
+//
+//  Created by 吳得人 on 2017/9/6.
+//  Copyright © 2017年 吳得人. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+import GameKit
+
+class MapViewController: UIViewController {
+    
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var mapView: GMSMapView!
+    var placesClient: GMSPlacesClient!
+    var zoomLevel: Float = 18.0
+    var circ = GMSCircle()
+    var nextCircleCorordinate: CLLocationCoordinate2D?
+    
+    
+    let button: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("START", for: .normal)
+        button.backgroundColor = .cyan
+        button.addTarget(self, action: #selector(setCircle), for: .touchUpInside)
+        return button
+    }()
+    
+    let nextButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Next", for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.9601936936, green: 0.4837267399, blue: 0.5332353115, alpha: 1)
+        button.addTarget(self, action: #selector(nextCircle), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        placesClient = GMSPlacesClient.shared()
+        let camera = GMSCameraPosition.camera(withLatitude: 25.039016,
+                                              longitude: 121.376042,
+                                              zoom: zoomLevel)
+        mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+        mapView.settings.myLocationButton = true
+        mapView.settings.compassButton = true
+        
+        //mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.isMyLocationEnabled = true
+        
+        // Add the map to the view, hide it until we've got a location update.
+        view.addSubview(mapView)
+        
+        mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 64).isActive = true
+        mapView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        mapView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -128).isActive = true
+        
+        mapView.isHidden = true
+        view.addSubview(button)
+        
+        button.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        button.leftAnchor.constraint(equalTo: mapView.leftAnchor).isActive = true
+        button.rightAnchor.constraint(equalTo: mapView.rightAnchor).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        
+        view.addSubview(nextButton)
+        
+        nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -64).isActive = true
+        nextButton.leftAnchor.constraint(equalTo: mapView.leftAnchor).isActive = true
+        nextButton.rightAnchor.constraint(equalTo: mapView.rightAnchor).isActive = true
+        nextButton.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        
+    }
+    
+    @objc func setCircle() {
+        if let lat = currentLocation?.coordinate.latitude , let lon = currentLocation?.coordinate.longitude {
+            //let circleCenter = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+           
+           let (nextLat, nextLon) = randomCoordinate(lat: lat, lon: lon)
+            
+            
+            nextCircleCorordinate = CLLocationCoordinate2D(latitude: nextLat, longitude: nextLon)
+            mapView.clear()
+            mapView.reloadInputViews()
+            circ = GMSCircle(position: nextCircleCorordinate!, radius: 50)
+            circ.fillColor = UIColor(red:0.35, green:0, blue:0, alpha:0.05)
+            circ.strokeColor = .red
+            circ.strokeWidth = 5
+            circ.map = mapView
+            
+            
+        }
+    }
+    
+    
+    @objc func nextCircle() {
+        let nextController = NextCircleViewController()
+        if let nextCircleCorordinate = self.nextCircleCorordinate {
+            
+            nextController.coordinator = nextCircleCorordinate
+           
+            
+            show(nextController, sender: nil)
+            
+        }
+    }
+    
+    func randomCoordinate(lat: CLLocationDegrees, lon: CLLocationDegrees ) -> (nextLat: Double, nextLon: Double) {
+        
+        let angleRandom = GKRandomDistribution(lowestValue: 0, highestValue: 360)
+        let newaAngleRandom = angleRandom.nextInt()
+        
+        let nextLat = Double(lat) + sin(Double(newaAngleRandom) / 180 * Double.pi) * 0.0013
+        let nextLon = Double(lon) + cos(Double(newaAngleRandom) / 180 * Double.pi) * 0.0013
+        return (nextLat, nextLon)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextCircleController = segue.destination as? NextCircleViewController
+        nextCircleController?.nextCircleCorordinate = self.nextCircleCorordinate!
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ NextCircleViewController.swift
@@ -0,0 +1,113 @@
+//
+//  NextCircleViewController.swift
+//  Battle Royale
+//
+//  Created by Vince Lee on 2017/9/7.
+//  Copyright © 2017年 吳得人. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+class NextCircleViewController: MapViewController {
+
+   
+    var coordinator = CLLocationCoordinate2D()
+    
+    let checkButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Check", for: .normal)
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(checkDist), for: .touchUpInside)
+        return button
+    }()
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        placesClient = GMSPlacesClient.shared()
+        let camera = GMSCameraPosition.camera(withLatitude: 25.039016,
+                                              longitude: 121.376042,
+                                              zoom: zoomLevel)
+        mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+        mapView.settings.myLocationButton = true
+        //mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.isMyLocationEnabled = true
+        
+        // Add the map to the view, hide it until we've got a location update.
+        view.addSubview(mapView)
+        
+        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        mapView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        mapView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -128).isActive = true
+        
+        mapView.isHidden = true
+        view.addSubview(button)
+        
+        button.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        button.leftAnchor.constraint(equalTo: mapView.leftAnchor).isActive = true
+        button.rightAnchor.constraint(equalTo: mapView.rightAnchor).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        
+        
+        view.addSubview(checkButton)
+        checkButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -64).isActive = true
+        checkButton.leftAnchor.constraint(equalTo: mapView.leftAnchor).isActive = true
+        checkButton.rightAnchor.constraint(equalTo: mapView.rightAnchor).isActive = true
+        checkButton.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        
+    }
+    
+    @objc func checkDist() {
+        let path = GMSMutablePath()
+        path.add(CLLocationCoordinate2D(latitude: (currentLocation?.coordinate.latitude)!, longitude: (currentLocation?.coordinate.longitude)!))
+        path.add(coordinator)
+        let polyline = GMSPolyline(path: path)
+        polyline.map = mapView
+        let distance =  polyline.path?.length(of: .geodesic) ?? 0
+        if distance <= 100 {
+            showAlertController(title: "SUCCESS")
+        } else {
+            showAlertController(title: "FAILD")
+        }
+    }
+    
+    @objc override func setCircle() {
+        circ = GMSCircle(position: coordinator, radius: 50)
+        circ.fillColor = UIColor(red:0.35, green:0, blue:0, alpha:0.05)
+        circ.strokeColor = .red
+        circ.strokeWidth = 5
+        circ.map = mapView
+    }
+    
+    func showAlertController(title: String) {
+        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let top = UIAlertAction(title: "OK", style: .destructive, handler: {
+            alert -> Void in
+            self.navigationController?.popViewController(animated: true)
+        })
+        
+        alertController.addAction(top)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
